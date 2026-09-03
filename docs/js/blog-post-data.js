c2b83a2c4a979193bc49ebaaab7a0c1a8caaa739
Brian Bergstrom <dulcetberg@gmail.com>
2026-09-02 20:45:21 -0500

    Add Chicago GWR crime analysis, replicating Cai (2025) PLOS One methodology on real 2023-2025 data

--- docs/js/blog-post-data.js
+++ docs/js/blog-post-data.js
@@ -4,6 +4,7 @@
 // whatever happens to be rendered on the current page. Update this
 // alongside blog.html (and rss.xml) whenever a new post goes up.
 window.BLOG_POST_DATES = [
+  "2026-09-03",
   "2026-08-31",
   "2026-08-31",
   "2026-08-31",
